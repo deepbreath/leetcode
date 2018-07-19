ded59653325a0227a96b427ddc1612574bd5031a
txby <2394497386@qq.com>
2018-07-19 21:59:55 +0800

    solution2

--- src/day1/TwoSumSolution2.java
+++ src/day1/TwoSumSolution2.java
@@ -9,15 +9,13 @@ public class TwoSumSolution2 {
 
         Map<Integer,Integer> map=new HashMap<>(16);
 
-
-        for (int i=0;i<nums.length;i++){
-       int compent=target-nums[i];
-       if (map.containsKey(compent)){
-           return new int[]{map.get(compent),i};
-       }
-          map.put(nums[i],i);
-
-        }
+     for (int i=0;i<nums.length;i++){
+         int result=target-nums[i];
+         if (map.containsKey(result)){
+             return new int[]{map.get(result),i};
+         }
+         map.put(nums[i],i);
+     }
 
         return new int[]{0,0};
     }
@@ -25,7 +23,7 @@ public class TwoSumSolution2 {
     public static void main(String[] args){
 
         TwoSumSolution2 twosum=new TwoSumSolution2();
-        int[] num={2,7,11,15};
+        int[] num={2,11,7,15};
         int target=9;
         int[] result=twosum.twoSum(num,target);
 
