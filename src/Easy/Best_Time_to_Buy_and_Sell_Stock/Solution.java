cf54fae6d349ba3c3b6650386879bab2f61801d4
admin <1900827748@qq.com>
2019-02-18 09:52:14 +0100

    finish

--- src/Easy/Best_Time_to_Buy_and_Sell_Stock/Solution.java
+++ src/Easy/Best_Time_to_Buy_and_Sell_Stock/Solution.java
@@ -1,4 +1,25 @@
 package Easy.Best_Time_to_Buy_and_Sell_Stock;
 
 public class Solution {
+    public static int maxProfit(int[] prices) {
+
+        int maxPrice=0;
+        for(int i=0;i<prices.length-1;i++){
+            for (int j=i+1;j<prices.length;j++){
+                int profit=prices[j]-prices[i];
+                if(profit>maxPrice){
+                    maxPrice=profit;
+                }
+            }
+
+        }
+
+        return maxPrice;
+    }
+
+    public static void main(String[] args){
+
+        int[] price={7,1,5,3,6,4};
+        System.out.println(maxProfit(price));
+    }
 }
