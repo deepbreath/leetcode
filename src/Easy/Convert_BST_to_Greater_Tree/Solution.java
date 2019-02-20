4b0fb69f59bc965a12d840ca6dc58d21f6fed589
admin <1900827748@qq.com>
2019-02-20 17:00:29 +0100

    BST 二叉搜索树

--- src/Easy/Convert_BST_to_Greater_Tree/Solution.java
+++ src/Easy/Convert_BST_to_Greater_Tree/Solution.java
@@ -1,4 +1,33 @@
 package Easy.Convert_BST_to_Greater_Tree;
 
+/**
+ * 只弄明白了这一个方法
+ */
 public class Solution {
+    private int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+
+
+        return root;
+
+    }
+
+    public static void main(String[] args){
+        Solution solution=new Solution();
+
+        TreeNode node=new TreeNode(5);
+
+        node.left=new TreeNode(2);
+
+        node.right=new TreeNode(13);
+
+        System.out.println(solution.convertBST(node));
+    }
 }
---
+++ src/Easy/Convert_BST_to_Greater_Tree/TreeNode.java
@@ -0,0 +1,10 @@
+package Easy.Convert_BST_to_Greater_Tree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        val=x;
+    }
+}
