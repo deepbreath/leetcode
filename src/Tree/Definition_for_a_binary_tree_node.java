932dab4ebb6611e86aaddc737e01f00f18c7cbe7
admin <1900827748@qq.com>
2020-03-01 09:29:16 +0100

    chongtu

--- src/Tree/Definition_for_a_binary_tree_node.java
+++ src/Tree/Definition_for_a_binary_tree_node.java
@@ -4,6 +4,6 @@ public class Definition_for_a_binary_tree_node {
 
     public static void main(String[] args){
 
-        System.out.println("34343");
+        System.out.println("3434ERrerte");
     }
 }
